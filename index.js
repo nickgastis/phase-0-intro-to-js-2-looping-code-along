d9e586776f8de9e3b0c0b7786e1660f3408f2b63
Nick Gastis <nickgastis1@gmail.com>
2023-01-27 14:55:51 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+
+function writeCards(names, event) {
+    const messages = []
+    for ( let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+      debugger
+   }
+   return messages;
+ }
+
+ writeCards(["Tom", "Sam", "Joe"], "birthday")
+
+//console.log(writeCards(["Tom", "Sam", "Joe"], "birthday"))
+const n = 12
+function countDown(n) {
+    let count = n;
+    while (count > -1 ) {
+        console.log(count--)
+    }
+}
